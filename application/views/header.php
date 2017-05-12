cc4e46abf274b4c50bbfdbcbe058d61cdf22f34c
crystal <jeremy85828@gmail.com>
2017-05-13 01:18:21 +0800

    WIP - added common header file

---
+++ application/views/header.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<title><?php echo $title;?></title>
+
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet" type="text/css">
+	
+	<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/script.js"></script>
+</head>
+
+<body>
+
+<div id="container">
+	<h1><?php echo $title; ?></h1>
+
+	<div id="body">
