a129cf7c70c51334f43a09d105d3cd243b446a9a
crystal <jeremy85828@gmail.com>
2017-05-30 04:09:16 +0800

    WIP - updated error controller name

--- application/controllers/Errors.php
+++ application/controllers/Errors.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Error extends CI_Controller {
+class Errors extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
